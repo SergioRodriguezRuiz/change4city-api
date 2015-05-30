70480e00813b897b64d3ec680535be824d96ba73
SergioRodriguezRuiz <serrodrui@gmail.com>
2015-05-30 18:33:53 +0200

    demo

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var methodOverride  = require("method-override");
-var mongoose        = require('mongoose');
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require('mongoose');
+var http = require ('http');             // For serving a basic web page.
 
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
 var uristring =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL ||
     'mongodb://localhost/HelloMongoose';
 
-// Connection to DB
-mongoose.connect(uristring, function(err, res) {
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+var theport = process.env.PORT || 5000;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+var text;
+mongoose.connect(uristring, function (err, res) {
     if (err) {
         console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-        text = 'hola';
-    } else {
         text = 'ola';
+    } else {
         console.log ('Succeeded connected to: ' + uristring);
+        text = 'hola';
     }
 });
 
-app.get('/', function(request, response) {
-  response.send(text);
+// Example Route
+var router = express.Router();
+router.get('/', function(req, res) {
+    res.send(text);
 });
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running on port:" + app.get('port'))
-})

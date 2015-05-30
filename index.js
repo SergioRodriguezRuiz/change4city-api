12bfbba57f131a226f019ef730c2bbe3c7459a8a
SergioRodriguezRuiz <serrodrui@gmail.com>
2015-05-30 18:16:06 +0200

    demo

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces');
+var bodyParser = require('body-parser');
+var methodOverride  = require("method-override");
+var mongoose        = require('mongoose');
+
+var uristring =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/HelloMongoose';
 
 app.set('port', (process.env.PORT || 5000));
 
+mongoose.connect(uristring, function (err, res) {
+    if (err) {
+        console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    } else {
+        console.log ('Succeeded connected to: ' + uristring);
+    }
+});
+
 app.get('/', function(request, response) {
-  response.send(cool());
+  response.send("holaaaaa");
 });
 
 app.listen(app.get('port'), function() {

c3b911d3a606bbb7e72bfafb19cbab745e970862
SergioRodriguezRuiz <serrodrui@gmail.com>
2015-05-30 18:49:29 +0200

    demo

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ var http = require ('http');             // For serving a basic web page.
 
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
-var uristring =
-    process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL ||
-    'mongodb://localhost/HelloMongoose';
+var uristring = 'mongodb://sergio:sergio1992@ds041032.mongolab.com:41032/heroku_app37166982';
 
 // The http server will listen to an appropriate port, or default to
 // port 5000.
-var theport = process.env.PORT || 5000;
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
@@ -37,6 +35,6 @@ router.get('/', function(req, res) {
 });
 app.use(router);
 // Start server
-app.listen(3000, function() {
-    console.log("Node server running on http://localhost:3000");
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });

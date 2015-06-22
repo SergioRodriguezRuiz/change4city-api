95487a7420b6a131f9b42098ba174ba8997087fc
sergio <serrodrui@gmail.com>
2015-06-22 13:04:08 +0200

    video model and controler

--- controllers/videos.js
+++ controllers/videos.js
@@ -13,3 +13,34 @@ exports.findAllVideos = function(req, res) {
     });
 };
 
+
+//GET by ID
+exports.findVideoId = function(req, res) {
+    console.log('GET ID');
+
+    Video.findById(req.params.id, function(err, vid) {
+        if(!err && vid) {
+            res.status(200).jsonp(vid);
+        } else {
+            res.send(500, err.message);
+        }
+    });
+};
+
+//POST - Insert a new Event in the DB
+exports.addVideo = function(req, res) {
+    console.log('POST');
+
+    var video = new Video({
+        url:                req.body.url,
+        id_vimeo:           req.body.id_vimeo
+    });
+
+    video.save(function(err, vid) {
+        if(err) {
+            return res.send(500, err.message);
+        }
+        res.status(200).jsonp(vid);
+    })
+};
+
